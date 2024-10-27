3b760a664024b118a7f6bbf5a18c894e729306d0
AhnEunJin <modic970515@gmail.com>
2024-10-27 20:44:35 +0900

    회원가입

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const indexRouter = require('./routes');
 const cors = require('cors');
 const app = express();
 
@@ -8,6 +9,7 @@ require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // req.body가 객체로 인식된다
+app.use('/api', indexRouter);
 
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose
