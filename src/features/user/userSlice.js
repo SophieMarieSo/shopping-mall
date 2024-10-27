4ae1c3a1fb219a2b96daba0ed7c1f54b3cc345e7
AhnEunJin <modic970515@gmail.com>
2024-10-27 21:47:44 +0900

    로그인 프론트엔드 기능 개발

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,35 +1,63 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-import { showToastMessage } from "../common/uiSlice";
-import api from "../../utils/api";
-import { initialCart } from "../cart/cartSlice";
+import { showToastMessage } from '../common/uiSlice';
+import api from '../../utils/api';
+import { initialCart } from '../cart/cartSlice';
 
 export const loginWithEmail = createAsyncThunk(
-  "user/loginWithEmail",
-  async ({ email, password }, { rejectWithValue }) => {}
+  'user/loginWithEmail',
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const resp = await api.post('/auth/login', { email, password });
+      return resp.data;
+    } catch (error) {
+      return rejectWithValue;
+    }
+  }
 );
 
 export const loginWithGoogle = createAsyncThunk(
-  "user/loginWithGoogle",
+  'user/loginWithGoogle',
   async (token, { rejectWithValue }) => {}
 );
 
 export const logout = () => (dispatch) => {};
 export const registerUser = createAsyncThunk(
-  "user/registerUser",
+  'user/registerUser',
   async (
     { email, name, password, navigate },
     { dispatch, rejectWithValue }
-  ) => {}
+  ) => {
+    try {
+      const resp = await api.post('/user', { email, password, name });
+      dispatch(
+        showToastMessage({
+          message: '회원가입에 성공했습니다',
+          status: 'success',
+        })
+      );
+      navigate('/login');
+
+      return resp.data.data;
+    } catch (error) {
+      dispatch(
+        showToastMessage({
+          message: '회원가입에 실패했습니다',
+          status: 'error',
+        })
+      );
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const loginWithToken = createAsyncThunk(
-  "user/loginWithToken",
+  'user/loginWithToken',
   async (_, { rejectWithValue }) => {}
 );
 
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState: {
     user: null,
     loading: false,
@@ -43,7 +71,31 @@ const userSlice = createSlice({
       state.registrationError = null;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.loading = false;
+        state.registrationError = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.registrationError = action.payload;
+      })
+      .addCase(loginWithEmail.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginWithEmail.fulfilled, (state, action) => {
+        state.loading = false;
+        state.loginError = null;
+        state.user = action.payload.user;
+      })
+      .addCase(loginWithEmail.rejected, (state, action) => {
+        state.loginError = action.payload;
+        state.loading = false;
+      });
+  },
 });
 export const { clearErrors } = userSlice.actions;
 export default userSlice.reducer;
