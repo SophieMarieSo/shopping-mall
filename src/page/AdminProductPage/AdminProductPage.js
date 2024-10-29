cb277ee7582659d606e1e2cf74b5b3afacc70b25
AhnEunJin <modic970515@gmail.com>
2024-10-29 22:02:14 +0900

    admin 페이지 상품 생성 기능

--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { Container, Button } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { useSearchParams, useNavigate } from "react-router-dom";
-import ReactPaginate from "react-paginate";
-import SearchBox from "../../common/component/SearchBox";
-import NewItemDialog from "./component/NewItemDialog";
-import ProductTable from "./component/ProductTable";
+import React, { useEffect, useState } from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+import SearchBox from '../../common/component/SearchBox';
+import NewItemDialog from './component/NewItemDialog';
+import ProductTable from './component/ProductTable';
 import {
   getProductList,
   deleteProduct,
   setSelectedProduct,
-} from "../../features/product/productSlice";
+} from '../../features/product/productSlice';
 
 const AdminProductPage = () => {
   const navigate = useNavigate();
@@ -19,21 +19,21 @@ const AdminProductPage = () => {
   const { productList, totalPageNum } = useSelector((state) => state.product);
   const [showDialog, setShowDialog] = useState(false);
   const [searchQuery, setSearchQuery] = useState({
-    page: query.get("page") || 1,
-    name: query.get("name") || "",
+    page: query.get('page') || 1,
+    name: query.get('name') || '',
   }); //검색 조건들을 저장하는 객체
 
-  const [mode, setMode] = useState("new");
+  const [mode, setMode] = useState('new');
 
   const tableHeader = [
-    "#",
-    "Sku",
-    "Name",
-    "Price",
-    "Stock",
-    "Image",
-    "Status",
-    "",
+    '#',
+    'Sku',
+    'Name',
+    'Price',
+    'Stock',
+    'Image',
+    'Status',
+    '',
   ];
 
   //상품리스트 가져오기 (url쿼리 맞춰서)
@@ -53,7 +53,9 @@ const AdminProductPage = () => {
 
   const handleClickNewItem = () => {
     //new 모드로 설정하고
+    setMode('new');
     // 다이얼로그 열어주기
+    setShowDialog(true);
   };
 
   const handlePageClick = ({ selected }) => {
@@ -61,46 +63,46 @@ const AdminProductPage = () => {
   };
 
   return (
-    <div className="locate-center">
+    <div className='locate-center'>
       <Container>
-        <div className="mt-2">
+        <div className='mt-2'>
           <SearchBox
             searchQuery={searchQuery}
             setSearchQuery={setSearchQuery}
-            placeholder="제품 이름으로 검색"
-            field="name"
+            placeholder='제품 이름으로 검색'
+            field='name'
           />
         </div>
-        <Button className="mt-2 mb-2" onClick={handleClickNewItem}>
+        <Button className='mt-2 mb-2' onClick={handleClickNewItem}>
           Add New Item +
         </Button>
 
         <ProductTable
           header={tableHeader}
-          data=""
+          data=''
           deleteItem={deleteItem}
           openEditForm={openEditForm}
         />
         <ReactPaginate
-          nextLabel="next >"
+          nextLabel='next >'
           onPageChange={handlePageClick}
           pageRangeDisplayed={5}
           pageCount={100}
           forcePage={searchQuery.page - 1}
-          previousLabel="< previous"
+          previousLabel='< previous'
           renderOnZeroPageCount={null}
-          pageClassName="page-item"
-          pageLinkClassName="page-link"
-          previousClassName="page-item"
-          previousLinkClassName="page-link"
-          nextClassName="page-item"
-          nextLinkClassName="page-link"
-          breakLabel="..."
-          breakClassName="page-item"
-          breakLinkClassName="page-link"
-          containerClassName="pagination"
-          activeClassName="active"
-          className="display-center list-style-none"
+          pageClassName='page-item'
+          pageLinkClassName='page-link'
+          previousClassName='page-item'
+          previousLinkClassName='page-link'
+          nextClassName='page-item'
+          nextLinkClassName='page-link'
+          breakLabel='...'
+          breakClassName='page-item'
+          breakLinkClassName='page-link'
+          containerClassName='pagination'
+          activeClassName='active'
+          className='display-center list-style-none'
         />
       </Container>
 
