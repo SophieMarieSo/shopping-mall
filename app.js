265aee4870b80a03d2f16c22a7039d81f98337ed
AhnEunJin <modic970515@gmail.com>
2024-11-01 01:02:58 +0900

    CORS  모두 허용

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ const cors = require('cors');
 const app = express();
 
 require('dotenv').config();
-const corsOptions = {
-  origin: 'http://localhost:3000', // 허용할 클라이언트 URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // 허용할 HTTP 메서드
-  allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // req.body가 객체로 인식된다
 
