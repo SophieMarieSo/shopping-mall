fa1c10330f8b9e8f570e3e144bda688aa45913c6
AhnEunJin <modic970515@gmail.com>
2024-11-01 00:22:14 +0900

    CORS 오류 해결

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const cors = require('cors');
 const app = express();
 
 require('dotenv').config();
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // req.body가 객체로 인식된다
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // 허용할 클라이언트 URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // 허용할 HTTP 메서드
+  allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
+};
+
+app.use(cors(corsOptions));
 app.use('/api', indexRouter);
 
 const mongoURI = process.env.MONGODB_URI_PROD;
